98d0a2825e226d545612b6e1aee9c72a09fbb661
Jason Sachs <jmsachs@gmail.com>
2013-09-22 11:49:43 -0700

    minor fixes, got orbit to work correctly

--- demos/gravity1/gravity1.js
+++ demos/gravity1/gravity1.js
@@ -1,10 +1,10 @@
 function Gravity1Simulation()
 {
     this.mu = 0.5;  // M * G
-    this.minR = 0.05;
-    this.velpos = [0,0.55,0.5,0];
+    this.minR = 0.09;
+    this.velpos = [0,1.2,0.5,0];
     this.solver = 'Trapezoidal';
-    this.initialEnergy = this.getKineticEnergy() + this.getPotentialEnergy();
+    this.initialEnergy = 1+this.getKineticEnergy() + this.getPotentialEnergy();
 }
 
 function weightedsum(A,x,len)
@@ -29,10 +29,12 @@ function dotpath(ctx,x,y,r)
     ctx.arc(x, y, r, 0, 2 * Math.PI, false);
 }
 function sumsq(x,y) { return x*x+y*y; }
+function hypot(x,y) { return Math.sqrt(x*x+y*y); }
 function drawbar(ctx,x0,dx,height,hmargin,e)
 {
     var dy = e*(height-2*hmargin);
-    ctx.fillRect(x0,height-hmargin-dy,dx,dy);
+    var ystart = dy > 0 ? height-hmargin-dy : hmargin;
+    ctx.fillRect(x0,ystart,dx,dy > 0 ? dy : -dy);
 }
 
 Gravity1Simulation.prototype = 
@@ -40,7 +42,7 @@ Gravity1Simulation.prototype =
     // 4-vector containing velx, vely, posx, posy
     calcDerivative: function(vx)
     {
-        var r = Math.sqrt(vx[2]*vx[2] + vx[3]*vx[3]);
+        var r = hypot(vx[2],vx[3])
         // |a| = F/M2 = 1/M2*(M1*M2*G/r/r) = M1*G/r/r
         // a = -|a|*x/|x| = -|a|*x/r
         var k = -this.mu/r/r/r;
@@ -69,11 +71,11 @@ Gravity1Simulation.prototype =
         }
     },
     getRadius: function() {
-        return Math.sqrt(sumsq(this.velpos[2],this.velpos[3]));
+        return hypot(this.velpos[2],this.velpos[3]);
     },
     getKineticEnergy: function() { return sumsq(this.velpos[0],this.velpos[1])/2; },
     getPotentialEnergy: function() {
-        return this.mu*(1.0/this.minR - 1.0/this.getRadius());
+        return this.mu*(-1.0/this.getRadius());
     },    
     getAngularMomentum: function() {
         // r x v = rx*vy - ry*vx
@@ -99,7 +101,7 @@ Gravity1Simulation.prototype =
         dotpath(ctx,x1,y1,2);
         ctx.fill();  
         
-        var e0=0.8 / this.initialEnergy;
+        var e0=0.2 / this.initialEnergy;
         var ke=e0*this.getKineticEnergy();
         var pe=e0*this.getPotentialEnergy();
         
@@ -122,26 +124,63 @@ Gravity1Simulation.prototype =
         var r = this.getRadius();
         var mu_e = [this.velpos[1]*h  - this.mu/r*this.velpos[2],
                     -this.velpos[0]*h - this.mu/r*this.velpos[3]];
-                    
+        var e = hypot(mu_e[0],mu_e[1])/this.mu;            
         ctx.beginPath();
-        var pathcoords = [];
-        for (var th = 0; th < 1; th += 0.01)
+        var penup = true;
+        var th_start = 0;
+        var th_stop = 1;
+        var done = false;
+        if (e > 1)
+        {
+            var rclosest = h*h/this.mu/(1+e);
+            var th_c = Math.atan2(mu_e[1],mu_e[0]);
+            var th_d = Math.PI-Math.acos(1/e);
+            th_start = (th_c-th_d+0.00001)/2/Math.PI;
+            th_stop = (th_c+th_d-0.00001)/2/Math.PI;
+            var rc = rmax*rclosest*(1+1/(e-1));
+            var rk = 2*rmax+rc;
+            var cx = cw/2 + rc*Math.cos(th_c);
+            var cy = ch/2 - rc*Math.sin(th_c);
+            ctx.moveTo(cx + rk*Math.cos(th_c-th_d),
+                       cy - rk*Math.sin(th_c-th_d));
+            ctx.lineTo(cx,cy);
+            ctx.lineTo(cx + rk*Math.cos(th_c+th_d),
+                       cy - rk*Math.sin(th_c+th_d));
+        }
+        th = th_start;
+        
+        while (true)
         {
             var th_i = th * 2 * Math.PI;
             var costh = Math.cos(th_i);
             var sinth = Math.sin(th_i);
             var r_i = h*h/(this.mu + (mu_e[0]*costh + mu_e[1]*sinth));
-            var rx = r_i*costh;
-            var ry = r_i*sinth;
-            var x = cw/2 + rmax*rx;
-            var y = ch/2 - rmax*ry;
-            pathcoords.push([th,r_i,rx,ry]);
-            if (th == 0)
-                ctx.moveTo(x,y);
+            if (r_i < 0)
+            {
+                penup = true;
+                break;
+            }
             else
-                ctx.lineTo(x,y);
+            {
+                var rx = r_i*costh;
+                var ry = r_i*sinth;
+                var x = cw/2 + rmax*rx;
+                var y = ch/2 - rmax*ry;
+                if (penup)
+                {
+                    ctx.moveTo(x,y);
+                    penup = false;
+                }
+                else
+                    ctx.lineTo(x,y);
+            }
+            
+            if (th >= th_stop)
+                break;
+            th += 0.05;
+            if (th > th_stop)
+                th = th_stop;
         }
-        return pathcoords;
     }
 }
 
