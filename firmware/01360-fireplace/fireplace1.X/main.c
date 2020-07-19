5278a5571c68140a867bca83dda665b1d24c0085
Jason Sachs <jmsachs@gmail.com>
2020-07-19 16:18:23 -0700

    add Apache license explicitly in key files

--- firmware/01360-fireplace/fireplace1.X/main.c
+++ firmware/01360-fireplace/fireplace1.X/main.c
@@ -5,6 +5,24 @@
 #include "mock_adc.h"
 #include "mock_uart.h"
 
+/*
+
+Copyright 2020 Jason M. Sachs
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+
+*/
+
 POKY_STATE poky_state;
 SHARED_MEMORY shmem;
 volatile int16_t something = 0;
