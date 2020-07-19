e962c874e64de352afb466c23a61eab86d88c5ee
Jason Sachs <jmsachs@gmail.com>
2020-07-19 14:19:14 -0700

    added config.c after all, to disable watchdog timer
    --HG--
    branch : 01360-fireplace

---
+++ firmware/01360-fireplace/fireplace2.X/config.c
@@ -0,0 +1,5 @@
+#pragma config WDTPOST = PS32768 // Watchdog Timer Postscaler bits (1:32,768)
+#pragma config WDTPRE = PR128 // Watchdog Timer Prescaler bit (1:128)
+#pragma config PLLKEN = ON // PLL Lock Enable bit (Clock switch to PLL source will wait until the PLL lock signal is valid.)
+#pragma config WINDIS = OFF // Watchdog Timer Window Enable bit (Watchdog Timer in Non-Window mode)
+#pragma config FWDTEN = OFF // Watchdog Timer Enable bit  
